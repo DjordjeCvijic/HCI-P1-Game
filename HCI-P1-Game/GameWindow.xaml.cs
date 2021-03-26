058a18d3c9907fc003326f3c7c8a71d9dcbc7bde
Djordje Cvijic <djordjecvijic@gmail.com>
2021-03-26 15:00:18 +0100

    Dvije forme i pitanja dodana

---
+++ HCI-P1-Game/GameWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HCI_P1_Game.Model;
+namespace HCI_P1_Game
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+            InitializeWindow();
+        }
+
+        private void InitializeWindow()
+        {
+            //za pravljenje novog using
+            //if (File.Exists(fileName))
+            //using(FileStream fs = File.Create(fileName)){
+            //    Byte[] title = new UTF8Encoding(true).GetBytes("pocetak");
+            //    fs.Write(title, 0, title.Length);
+            // } a za dodavanje  File.AppendAllText(fileName, "novooo");
+
+
+            List<Question> allQuestions = new List<Question>();
+            string fileName = "questions.txt";
+            
+            string[] allLines = File.ReadAllLines(fileName);
+            foreach (string line in allLines)
+            {
+                Console.WriteLine(line);
+                LBForQuestion.Items.Add(line);
+            }
+        }
+    }
+}
---
+++ HCI-P1-Game/Model/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCI_P1_Game.Model
+{
+    class Question
+    {
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public string QuestionText { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string CorrectAnswer { get; set; }
+    
+    }
+}
