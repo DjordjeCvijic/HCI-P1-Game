29bd5eed1ad111a095f8e4459e9b9ebf49a39577
Djordje Cvijic <djordjecvijic@gmail.com>
2021-03-27 20:44:49 +0100

    Odradjeno prikazivanja pitanja i odgovora

--- HCI-P1-Game/GameWindow.xaml.cs
+++ HCI-P1-Game/GameWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace HCI_P1_Game
     /// </summary>
     public partial class GameWindow : Window
     {
+        private int CurrentQuestion=1;
+        private Random random = new Random();
         public GameWindow()
         {
             InitializeComponent();
@@ -34,15 +36,54 @@ namespace HCI_P1_Game
             // } a za dodavanje  File.AppendAllText(fileName, "novooo");
 
 
-            List<Question> allQuestions = new List<Question>();
+            List<Question> questionList1 = new List<Question>();
+            List<Question> questionList2 = new List<Question>();
+            List<Question> questionList3 = new List<Question>();
             string fileName = "questions.txt";
             
             string[] allLines = File.ReadAllLines(fileName);
+            string []lineParts;
             foreach (string line in allLines)
             {
-                Console.WriteLine(line);
-                LBForQuestion.Items.Add(line);
+                lineParts = line.Split(';');
+                Question q = new Question
+                {
+                    Id = int.Parse(lineParts[0]),
+                    Category = int.Parse(lineParts[1]),
+                    QuestionText = lineParts[2],
+                    Answer1 = lineParts[3],
+                    Answer2 = lineParts[4],
+                    Answer3 = lineParts[5],
+                    Answer4 = lineParts[6],
+                    CorrectAnswer = lineParts[3]
+
+                };
+                if (q.Category == 1)
+                    questionList1.Add(q);
+                else if (q.Category == 2)
+                    questionList2.Add(q);
+                else
+                    questionList3.Add(q);
             }
+
+            //int questionNumber = random.Next(questionList1.Count )+1;
+            int questionNumber = 14;// ovo mora rijesiti
+            TbQuestion.Text = questionList1[questionNumber].QuestionText;
+            BtnAnswerA.Content = questionList1[questionNumber].Answer1;
+            BtnAnswerA.Tag = questionList1[questionNumber].Answer1;
+            BtnAnswerB.Content = questionList1[questionNumber].Answer2;
+            BtnAnswerB.Tag = questionList1[questionNumber].Answer2;
+            BtnAnswerC.Content = questionList1[questionNumber].Answer3;
+            BtnAnswerC.Tag = questionList1[questionNumber].Answer3;
+            BtnAnswerD.Content = questionList1[questionNumber].Answer4;
+            BtnAnswerD.Tag = questionList1[questionNumber].Answer4;
+
+        }
+
+        private void BtnAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            string answer = (sender as Button).Tag.ToString();
+            TbAnswer.Text = answer;
         }
     }
 }
